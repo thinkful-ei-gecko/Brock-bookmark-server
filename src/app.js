679ee9bcefe66afd8d16135b4f0aae2570261a71
Brock Boutwell <brockboutwell@gmail.com>
2019-09-29 15:22:05 -0400

    revision changes to app.js

--- src/app.js
+++ src/app.js
@@ -27,6 +27,6 @@ app.use(morgan(morganOption));
 app.use(helmet());
 app.use(cors());
 app.use(express.json());
-app.use(bookmarkRouter);
+app.use('/bookmarks', bookmarkRouter);
 
 module.exports = app;
